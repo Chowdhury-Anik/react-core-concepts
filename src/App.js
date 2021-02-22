d8bc4b60a1754f0f7b2f488105f7769dbe8404d2
Chowdhury-Anik <bloggeranik1986@gmail.com>
2021-02-23 04:05:56 +0600

    first react app commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
       
+        <h1>My React App</h1>
+        <p>First time react practice!</p>
+        <Person name="Anik Chowdhury" job="Learner"></Person>
+        <Person name="Jhankar Mahabub" job="Instructor"></Person>
+        <Person name="joy Dev" job="Job Holder"></Person>
+        
       </header>
     </div>
   );
 }
+ 
+   function Person(props){
+     // you can apply style by declaring a variable
+     const PersonStyle={
+       border:'2px solid gold',
+       margin: '10px',
+       width: '600px'
+
+     }
+  return(<div style = {PersonStyle}>
+    <h2>Name: {props.name}</h2>
+    <h4>Profession: {props.job}</h4>
+  </div>) 
+  }
+
 
 export default App;
